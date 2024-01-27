f43c6c1d14b14f73aad65ffaa5193f8093e6985f
mwallace98 <mattwallace140@gmail.com>
2024-01-26 21:46:42 -0500

    wip

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,29 @@
 // build your `/api/resources` router here
+const express = require('express')
+const router = express.Router()
+const Router = require('./model')
+
+router.use(express.json())
+
+
+router.post('/', async (req,res,next) => {
+    try { 
+        res.json('post new resource')
+    } catch (err){
+        next(err)
+    }
+})
+
+router.get('/',(req,res,next) => {
+    try { 
+        res.json('get new resources')
+    } catch (err){
+        next(err)
+    }
+})
+
+
+
+module.exports = router
+
+
--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const projectRouter = require('./project/router')
 const taskRouter = require('./task/router')
+const resourcesRouter = require('./resource/router')
 const server = express()
 server.use(express.json())
 
 server.use('/api/projects',projectRouter)
 server.use('/api/tasks', taskRouter)
+server.use('/api/resources',resourcesRouter)
 
 server.use('*', (req,res,next)=> {
     next({status:404,message:'not found'})
